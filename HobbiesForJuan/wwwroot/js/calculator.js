8565d3ec17b8451d26990be3be76f0b84722caa4
jforellana <mrjfob@byu.edu>
2024-02-09 23:52:39 -0700

    Add project files.

---
+++ HobbiesForJuan/wwwroot/js/calculator.js
@@ -0,0 +1,19 @@
+﻿namespace HobbiesForJuan.wwwroot.js
+{
+    public class calculator
+    {
+        $(document).ready(function() {
+            $("#calculate").click(function () {
+                // Get the values from input fields
+                var value1 = $("#hours").val();
+                var value2 = 50;
+
+                // Convert values to numbers and perform multiplication
+                var result = parseFloat(value1) * parseFloat(value2);
+
+                // Set the result in the result input field
+                $("#result").val(result);
+            });
+        });
+    }
+}
